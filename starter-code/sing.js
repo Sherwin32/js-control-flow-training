6ee604991d0762cbe60699760b24884f57beacf6
Sherwin32 <sherwin.shann@gmail.com>
2017-09-27 20:35:12 -0700

    Fixed the case for 2 bottles. Get 1 'bottle' after handing out one bottle.

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,17 +1,23 @@
 console.log("sing.js loaded");
 let bottleInput = parseInt(prompt("Please let me know how many bottles you want."));
 for(var i = bottleInput; i>0; i--){
-	if(i === 1){
+	if(i === 2){
+		console.log(
+			`${i} bottles of beer on the wall,
+${i} bottles of beer!
+Take one down and pass it around,
+${i-1} bottle of beer on the wall!`)
+	}else if(i === 1){
 		console.log(
 			`${i} bottle of beer on the wall,
-{i} bottle of beer!
+${i} bottle of beer!
 Take one down and pass it around,
 No more bottles...`)
 	}else{
 		console.log(
 			`${i} bottles of beer on the wall,
-{i} bottles of beer!
+${i} bottles of beer!
 Take one down and pass it around,
-{i-1} bottles of beer on the wall!`)
+${i-1} bottles of beer on the wall!`)
 	}
 }
